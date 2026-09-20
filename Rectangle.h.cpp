d21abb0eeb5047113f79a86f8cd7ce1c8f3b052e
mariawana <221686344+mariawana@users.noreply.github.com>
2026-09-20 12:38:06 +0800

    Create Rectangle.h.cpp

---
+++ Rectangle.h.cpp
@@ -0,0 +1,21 @@
+#include "Rectangle.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Rectangle r1;
+    Rectangle r2;
+
+    r1.setWidth(5.0);
+    r1.setLength(10.0);
+
+    r2.setWidth(3.0);
+    r2.setLength(9.3);
+
+    cout << "r1 area is: " << r1.getArea() << endl;
+    cout << "r2 area is: " << r2.getArea() << endl;
+
+    return 0;
+}
